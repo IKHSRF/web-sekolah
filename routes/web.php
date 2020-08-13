bad2eede956aa6f1c4c3af141a6a53723f5e9556
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2020-08-13 12:52:42 +0700

    migrating jurusan, galery and prestasi table

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('prestasis', 'PrestasiController');
+Route::resource('jurusans', 'JurusanController');
+Route::resource('gallerys', 'GalleryController');
