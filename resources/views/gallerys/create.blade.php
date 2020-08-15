4ce32217eb99a45220d5daf2f710249d9661f8aa
Ikhsan Arfian Nugraha <ikhsan3568@gmail.com>
2020-08-15 12:06:32 +0700

    adding create & edit gallery

---
+++ resources/views/gallerys/create.blade.php
@@ -0,0 +1,54 @@
+@extends('gallerys.layout')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Tambah Galeri</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('gallerys.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+<form action="{{ route('gallerys.store') }}" method="POST">
+    @csrf
+
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Judul:</strong>
+                <input type="text" name="nama_galeri" class="form-control" placeholder="Judul">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Detail:</strong>
+                <textarea class="form-control" style="height:150px" name="detail_galeri" placeholder="Detail"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Upload Foto</strong>
+                <input type="file">Upload</input>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+
+</form>
+@endsection
---
+++ resources/views/gallerys/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('gallerys.layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit Galeri</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('gallerys.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('gallerys.update',$gallery->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Judul:</strong>
+                    <input type="text" name="nama_galeri" value="{{ $gallery->nama_galeri }}" class="form-control" placeholder="Name">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Detail:</strong>
+                    <textarea class="form-control" style="height:150px" name="detail_galeri" placeholder="Detail">{{ $gallery->detail_galeri }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Upload Foto</strong>
+                <input type="file">Upload</input>
+            </div>
+        </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+
+    </form>
+@endsection
